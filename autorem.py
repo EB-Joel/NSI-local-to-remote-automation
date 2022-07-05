bebe8bf8db4d450b1d3bb31b8e6cda876ca7c7b4
Eric Berroa <ericberroa@live.com>
2022-07-05 14:28:39 -0400

    Update autorem.py
    updated IP

--- autorem.py
+++ autorem.py
@@ -1,11 +1,11 @@
 import os
 import paramiko
+import sys
 
 ssh = paramiko.SSHClient() 
-ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(server, username=username, password=password)
+ssh.connect(server=172.16.5.36 , username=username, password=password)
 sftp = ssh.open_sftp()
-sftp.put(localpath, remotepath)
+sftp.put(f'F:\{sys.argv[1]}', 'remotepath')
 sftp.close()
 ssh.close()
